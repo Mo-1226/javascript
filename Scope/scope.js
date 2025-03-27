13328d4ed518742f6daf60fc322f93438234de76
Mo-1226 <mohammadahmed2026@gmail.com>
2025-03-27 11:45:28 -0400

    block scope vs function scope

---
+++ Scope/scope.js
@@ -0,0 +1,33 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+// Block scope
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+
+console.log(blockLet);
+
+// Global scope
+// console.log(blockVar); // Output: "I'm a global variable"
+// console.log(blockLet); // Output: "I'm also global, but scoped with let"
+// console.log(blockConst); // Output: "I'm a global constant"
+
+
+
+function show(){
+    var functionVar = "I am a block scoped var ";
+    let functionLet = "I am a block scoped let";
+    const functionconst = "I am a block scoped const";
+}
+
+show();
+
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionconst);
